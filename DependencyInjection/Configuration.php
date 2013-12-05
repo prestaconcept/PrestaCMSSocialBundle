119f809c0dcfd5208d20eb39286329fe44087547
Benoît Lévêque <benoit@leveque.me>
2013-12-05 15:53:50 +0100

    #2: bundle set-up

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the PrestaCMSSocialBundle
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSSocialBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * @author Benoît Lévêque <bleveque@prestaconcept.net>
+ */
+class Configuration implements ConfigurationInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('presta_cms_social');
+
+        return $treeBuilder;
+    }
+}
