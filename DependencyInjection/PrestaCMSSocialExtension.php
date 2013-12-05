5f23cd9a135edf29c706b368cdc919c09edb60dd
Benoît Lévêque <benoit@leveque.me>
2013-12-05 16:42:09 +0100

    Add an Extension to process bundle configuration

---
+++ DependencyInjection/PrestaCMSSocialExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Presta\CMSSocialBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class PrestaCMSSocialExtension extends Extension
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $config);
+    }
+}
