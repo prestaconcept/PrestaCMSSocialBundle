5209cf361eb9ad4db1c4ba5039b3fdb00d5a21b8
Benoît Lévêque <benoit@leveque.me>
2013-12-10 16:33:02 +0100

    Add phpunit for unit testing of this bundle

---
+++ Tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the PrestaCMSSocialBundle.
+ *
+ * (c) PrestaConcept <http://www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
+}
+
+require $autoloadFile;
